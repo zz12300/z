c72d78137a751cb3eb65e54f74f1e2bdae1c717c
zz12300 <127149522+zz12300@users.noreply.github.com>
2025-06-13 03:37:58 +0800

    创建 intolive.js

---
+++ js/intolive.js
@@ -0,0 +1,18 @@
+更新日期：2025-06-03
+脚本作者：@ddm1023
+电报频道：https://t.me/ddm1023
+使用声明：⚠️仅供参考，🈲转载与售卖！
+使用说明：如果脚本无效，请先排除是否脚本冲突
+特别说明：此脚本可能会导致App Store无法登录ID
+解决方法：关[MITM][脚本][代理工具]方法选一即可
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/buy\.itunes\.apple\.com\/verifyReceipt$ url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/iTunes.js
+
+[mitm]
+hostname = buy.itunes.apple.com
+
+*************************************/
+  'intolive': { cm: 'timea', hx: 'hxpda', id: "me.imgbase.intolive.proSubYearly", latest: "ddm1023" }, //intolive-实况壁纸制作器
